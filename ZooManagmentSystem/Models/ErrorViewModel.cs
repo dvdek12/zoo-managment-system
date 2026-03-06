fea00a55bae8228f209b4eb6fbd106c6cac0388f
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-03-06 16:12:22 +0100

    Dodaj pliki projektów.

---
+++ ZooManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZooManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
