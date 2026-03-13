b15121d0b0a01e2ffcbd9b7b923a0bddb0ff3d62
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-03-13 17:43:42 +0100

    Dodano uzycie DbContext w głownym pliku - podpiecie bazy

--- ZooManagmentSystem/Program.cs
+++ ZooManagmentSystem/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ZooManagmentSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ZooManagmentSystemContext' not found.")));
 
 var app = builder.Build();
 
