1ea101d98e7aab0df55c4a0fdc3be3e6a2c9ba72
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-03-13 17:45:15 +0100

    dodano dziedziczenie FIX

--- ZooManagmentSystem/Models/MammalModel.cs
+++ ZooManagmentSystem/Models/MammalModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace ZooManagmentSystem.Models
 {
-    public class MammalModel
+    public class MammalModel : AnimalModel
     {
         public string Gender {  get; set; }
         public DateTime? BirthDate { get; set; }
