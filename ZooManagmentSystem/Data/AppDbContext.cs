5742a7099c6668ce7f8252aef3ab48fa5d1b0cbb
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-03-18 19:26:36 +0100

    Added DbSets

--- ZooManagmentSystem/Data/AppDbContext.cs
+++ ZooManagmentSystem/Data/AppDbContext.cs
@@ -12,5 +12,11 @@ namespace ZooManagmentSystem.Data
         public DbSet<Models.Employee> Employees { get; set; }
         public DbSet<Models.MammalModel> Mammals { get; set; }
         public DbSet<Models.TaskModel> Tasks { get; set; }
+        public DbSet<Models.EnclosureModel> Enclosures { get; set; }
+
+        public DbSet<Models.AnimalHistoryModel> AnimalHistories { get; set; }
+
+        public DbSet<Models.FeedingPlanRaportModel> FeedingPlanRaports { get; set; }
+        public DbSet<Models.AnimalHistoryRaportModel> AnimalHistoryRaports { get; set; }
     }
 }
