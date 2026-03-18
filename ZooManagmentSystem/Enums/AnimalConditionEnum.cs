e5d001e2a4de208d9a9784a386a39996948b3795
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-03-18 19:27:07 +0100

    Added AnimalHistoryModel

---
+++ ZooManagmentSystem/Enums/AnimalConditionEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace ZooManagmentSystem.Enums
+{
+    public enum AnimalConditionEnum
+    {
+        Critical,
+        Healthy,
+        Sick,
+        Injured,
+        Recovering
+    }
+}
---
+++ ZooManagmentSystem/Models/AnimalHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿using ZooManagmentSystem.Enums;
+
+namespace ZooManagmentSystem.Models
+{
+    public class AnimalHistoryModel : ModelPrototype
+    {
+        public AnimalModel? Animal { get; set; }
+        public AnimalConditionEnum ConditionAdmission { get; set; }
+        public float Temperature { get; set; }
+
+        public float Weight { get; set; }
+
+        public bool IsVacinated { get; set; }
+    }
+}
