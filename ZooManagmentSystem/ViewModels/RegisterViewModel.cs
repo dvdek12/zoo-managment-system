ddb278b187db6a6e57e210202bc144ca141049e1
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-04-23 15:06:55 +0200

    dodanie reszty pól

--- ZooManagmentSystem/ViewModels/RegisterViewModel.cs
+++ ZooManagmentSystem/ViewModels/RegisterViewModel.cs
@@ -4,6 +4,12 @@ namespace ZooManagmentSystem.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime BirthDay { get; set; }
         [Required, EmailAddress]
         public string Email { get; set; }
         [Required, DataType(DataType.Password)]
