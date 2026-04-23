7e27d2c320025f7989ea2965d7bbdb8c1abb8136
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-04-23 19:37:49 +0200

    test gorilla hub

---
+++ ZooManagmentSystem/Hubs/GorillaHealthNot.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZooManagmentSystem.Hubs
+{
+    public class GorillaHealthNot : Hub
+    {
+        public async Task SendGorillaHealthNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveGorillaHealthNotification", message);
+        }
+    }
+}
