694b3c22bcd0dba889963f7c78296deed94294a3
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-04-24 12:33:28 +0200

    Kontroler obsługujący External API ninja

---
+++ ZooManagmentSystem/Controllers/ExternalAnimalsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZooManagmentSystem.Controllers
+{
+    [Route("api/external-animals")]
+    public class ExternalAnimalsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ExternalAnimalsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetAnimal(string name)
+        {
+            var client = _httpClientFactory.CreateClient("ExternalAnimalsClient");
+
+            var response = await client.GetAsync($"animals?name={name}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+
+            return StatusCode((int)response.StatusCode, "Error fetching animal data from external API.");
+        }
+    }
+}
