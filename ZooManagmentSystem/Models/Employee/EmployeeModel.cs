4abad999a2b557a56bb8b428ce94980eb00c1c54
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-05-14 14:54:21 +0200

    added foreign key IconId to animal and employee

--- ZooManagmentSystem/Models/Employee/EmployeeModel.cs
+++ ZooManagmentSystem/Models/Employee/EmployeeModel.cs
@@ -17,5 +17,7 @@ namespace ZooManagmentSystem.Models.Employee
 
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
+        public int IconId { get; set; }
+        public IconModel? Icon { get; set; }
     }
 }
