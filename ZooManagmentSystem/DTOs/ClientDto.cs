aa5e83c967724b125da06c80cce8b5cde8e7acbe
unknown <miczejko.kacper@gmail.com>
2026-05-14 17:52:16 +0200

    Added client controller

--- ZooManagmentSystem/DTOs/ClientDto.cs
+++ ZooManagmentSystem/DTOs/ClientDto.cs
@@ -2,7 +2,7 @@
 
 namespace ZooManagmentSystem.DTOs
 {
-    public class ClientDto
+    public class ClientRegisterDto
     {
         [Required, StringLength(50)]
         public string FirstName { get; set; }
@@ -14,5 +14,22 @@ namespace ZooManagmentSystem.DTOs
         public string Email { get; set; }
         [Required, DataType(DataType.Password)]
         public string Password { get; set; }
+    }
+    public class ClientDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+    public class ClientUpdateDto
+    {
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public string? PhoneNumber { get; set; }
+
     }
 }
