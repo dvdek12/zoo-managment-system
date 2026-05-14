fc1ea99e61ad0272b39a066862cacb7c76f68662
Przemyslaw Dudek <przemekdudek321@gmail.com>
2026-05-14 15:37:43 +0200

    added IconController with Upload method POST

---
+++ ZooManagmentSystem/Controllers/IconController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZooManagmentSystem.Data;
+using ZooManagmentSystem.Models;
+
+namespace ZooManagmentSystem.Controllers
+{
+    [Route("Icon")]
+    public class IconController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public IconController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if(file == null || file.Length == 0) return BadRequest("No file uploaded.");
+
+            using var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+
+            var icon = new IconModel
+            {
+                Name = file.FileName,
+                ContentType = file.ContentType,
+                ImageData = stream.ToArray()
+            };
+
+            _context.Icons.Add(icon);
+            await _context.SaveChangesAsync();
+
+            return Ok(new {icon.id, message = "Icon uploaded successfully!"});
+
+        }
+    }
+}
