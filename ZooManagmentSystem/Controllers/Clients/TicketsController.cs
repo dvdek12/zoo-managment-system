132865b68fba68978b6fba29f78990b9c73b5cc7
unknown <miczejko.kacper@gmail.com>
2026-05-15 13:57:23 +0200

    Changed what is sent by api in ticketController

--- ZooManagmentSystem/Controllers/Clients/TicketsController.cs
+++ ZooManagmentSystem/Controllers/Clients/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -25,28 +26,60 @@ namespace ZooManagmentSystem.Controllers.Clients
         // GET: tickets
         
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TicketModel>>> GetTickets()
+        public async Task<ActionResult<IEnumerable<TicketsAllDto>>> GetTickets()
         {
-            return Ok(await _context.Tickets.ToListAsync());
+            var tickets = await _context.Tickets.Include(t => t.EntryTypes).ToListAsync();
+            var ticketList = new List<TicketsAllDto>();
+
+            foreach (var ticket in tickets)
+            {
+                var ticketDto = new TicketsAllDto
+                {
+                    Id = ticket.id,
+                    ClientId = ticket.ClientId,
+                };
+                foreach (var entry in ticket.EntryTypes)
+                {
+                    var entryType = await _context.EntryTypes.FindAsync(entry.EntryTypeId);
+                    ticketDto.EntryTypes.Add(entryType.TypeName, entry.Quantity);
+                }
+                ticketList.Add(ticketDto);
+            }
+
+            return Ok(ticketList);
         }
             
         // GET: /tickets/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<TicketModel>> GetTicket(int id)
+        public async Task<ActionResult<TicketDetailsDto>> GetTicket(int id)
         {
-            var ticketModel = await _context.Tickets.FindAsync(id);
+            var ticketModel = await _context.Tickets.Include(t => t.EntryTypes).FirstOrDefaultAsync(t => t.id == id);
 
             if (ticketModel == null)
             {
                 return NotFound();
             }
 
-            return Ok(ticketModel);
+            var ticketDetails = new TicketDetailsDto
+            {
+                ClientId = ticketModel.ClientId,
+                PurchaseDate = ticketModel.PurchaseDate,
+                ValidUntil = ticketModel.ValidUntil,
+                Price = ticketModel.Price,
+                EntryTypes = new Dictionary<string, int>()
+            };
+            foreach(TicketEntryTypeModel ticketEntry in ticketModel.EntryTypes)
+            {
+                var entryType = await _context.EntryTypes.FindAsync(ticketEntry.EntryTypeId);
+                ticketDetails.EntryTypes.Add(entryType.TypeName, ticketEntry.Quantity);
+            }
+
+            return Ok(ticketDetails);
         }
 
         // GET: /tickets/forClient/5
         [HttpGet("forClient/{clientId}")]
-        public async Task<ActionResult<IEnumerable<TicketModel>>> GetTicketsForClient(int clientId)
+        public async Task<ActionResult<IEnumerable<TicketsForClientDto>>> GetTicketsForClient(int clientId)
         {
             var tickets = await _context.Tickets
                 .Where(t => t.ClientId == clientId)
@@ -58,7 +91,23 @@ namespace ZooManagmentSystem.Controllers.Clients
                 return NotFound();
             }
 
-            return Ok(tickets);
+            var ticketList = new List<TicketsAllDto>();
+
+            foreach(TicketModel ticket in tickets)
+            {
+                var ticketDto = new TicketsAllDto
+                {
+                    Id = ticket.id,
+                };
+                foreach (TicketEntryTypeModel entry in ticket.EntryTypes)
+                {
+                    var entryType = await _context.EntryTypes.FindAsync(entry.EntryTypeId);
+                    ticketDto.EntryTypes.Add(entryType.TypeName, entry.Quantity);
+                }
+                ticketList.Add(ticketDto);
+            }
+
+            return Ok(ticketList);
         }
 
         // POST: tickets
