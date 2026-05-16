b6d8d5f870a26eb42aa7f569fdbec4734238cf16
unknown <miczejko.kacper@gmail.com>
2026-05-16 18:43:31 +0200

    Made migration with new report model

--- ZooManagmentSystem/Models/Raport/FeedingPlanRaportModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ZooManagmentSystem.Models.Employee;
-
-namespace ZooManagmentSystem.Models.Raport
-{
-    public class FeedingPlanRaportModel : RaportModel
-    {
-        public EmployeeModel? EmployeeAssigned { get; set; }
-    }
-}
